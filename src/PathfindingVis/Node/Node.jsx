ca18f38c2a0a571a773c146a5bf5b948ef04495d
ryan-r3v <54917819+ryan-r3v@users.noreply.github.com>
2022-05-18 15:41:41 -0500

    fixed merge problem

--- src/PathfindingVis/Node/Node.jsx
+++ src/PathfindingVis/Node/Node.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {Component} from 'react';
 
 import './Node.css';
@@ -32,39 +31,4 @@ export default class Node extends Component {
         onMouseUp={() => onMouseUp()}></div>
     );
   }
-=======
-import React, {Component} from 'react';
-
-import './Node.css';
-
-export default class Node extends Component {
-  render() {
-    const {
-      row,
-      col,
-      isStart,
-      isFinish,
-      isWall,
-      onMouseDown,
-      onMouseEnter,
-      onMouseUp,
-    } = this.props;
-    const extraClassName = isFinish
-      ? 'finishNode'
-      : isStart
-      ? 'startNode'
-      : isWall
-      ? 'wallNode'
-      : '';
-
-    return (
-      <div
-        id={`node-${row}-${col}`}
-        className={`node ${extraClassName}`}
-        onMouseDown={() => onMouseDown(row, col)}
-        onMouseEnter={() => onMouseEnter(row, col)}
-        onMouseUp={() => onMouseUp()}></div>
-    );
-  }
->>>>>>> 44a2c2f16cc4e66cb61c5ca79cf5fa0b76d3a251
 }
